af92e226371414a6a2fcdb3ce82c98740cb7a428
Rolandibirix7 <rolanlopez1304@gmail.com>
2026-05-30 14:16:21 -0600

    Crear modelos, migraciones y controladores iniciales

---
+++ database/migrations/2026_05_30_193914_create_herramientas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('herramientas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion')->nullable();
+            $table->enum('estado', ['disponible', 'prestada', 'mantenimiento'])->default('disponible');
+            $table->string('codigo_inventario')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('herramientas');
+    }
+};
---
+++ database/migrations/2026_05_30_193928_create_prestamos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('herramienta_id')->constrained();
+            $table->date('fecha_prestamo');
+            $table->date('fecha_devolucion_esperada');
+            $table->date('fecha_devolucion_real')->nullable();
+            $table->enum('estado', ['activo', 'devuelto', 'retrasado'])->default('activo');
+            $table->text('observaciones')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
